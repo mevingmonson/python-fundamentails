0f25556f19c2641c7decb5d718e4f3e07e087bb6
Mevin G Monson <mevin@goodbits.in>
2021-05-27 15:50:11 +0530

    getting to know python

---
+++ main.py
@@ -0,0 +1,116 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+# Use a breakpoint in the code line below to debug your script.
+# Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+# varaibles
+# a = 10
+# a = "mevin"
+
+# Receiving input
+# a = input("enter name")
+# print("hello " + a)
+
+# Type conversion
+# first = float(input("First:"))
+# second = float(input("Second:"))
+# res = first + second
+# print("sum=" + str(res))
+
+
+# strings
+# course = "python for beginners"
+# print(course.upper())
+# print(course)
+# print(course.find("y"))
+# print(course.find("Y"))
+# print(course.replace("for", "4"))
+# print("python" in course)
+
+# arithmetic operations
+# print(10+3)
+# print(10-3)
+# print(10*3)
+# print(10**3)
+# print(10/3)
+# print(10//3)
+
+# comparison operators
+# > >= < <= == !=
+
+# logical operators
+# and or not
+
+# if statements
+
+# temp = 10
+# if temp > 30:
+#     print("it's a hot day")
+#     print("Drink plenty of water")
+# elif temp > 20:
+#     print("It's a nice day")
+# elif temp > 10:
+#     print("It's a bit cold")
+# else:
+#     print("Its very cold")
+# print("Done")
+
+
+# while
+# i = 1
+# while i <= 5:
+#     print(i * "*")
+#     i += 1
+
+
+# list
+# names = ["John", "Bob", "Mosh", "Sam", "Mike"]
+# print(names[0])
+# print(names[-1])
+# print(names[-2])
+# print(names[0:2])
+# print(names)
+
+# list methods
+
+# numbers=[1,5,6,3,2,7,5,4,]
+# numbers.append(100)
+# print(numbers)
+# numbers.insert(2,200)
+# print(numbers)
+# numbers.remove(100)
+# print(numbers)
+# print(len(numbers))
+# print(50 in numbers)
+# numbers.clear()
+# print(numbers)
+
+# for loop
+# numbers= [ 1,5,3,7]
+# for item in numbers:
+#     print(item)
+#
+#  while
+# i = 0
+# while i<len(numbers):
+#     print(numbers[i])
+#     i+=1
+
+# range
+numbers = range(10)
+numbers = range(5, 10)
+numbers = range(5, 10, 2)
+for num in numbers:
+    print(num)
+
+# tuples
